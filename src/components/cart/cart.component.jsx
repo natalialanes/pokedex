3b376a96a3bc519b6a80f2402459c2cdd27a0ef2
Natalia Lanes <natalialanes@MB-004507.local>
2021-04-25 20:37:52 -0300

    added header and cart components

---
+++ src/components/cart/cart.component.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useCart, useModal, useTheme } from '../../hooks'
+import './cart.style.css'
+import { CartItem } from './components/cart-item/cart-item.component'
+
+const Cart = () => {
+  const { showSuccessModal } = useModal()
+  const { cartPokemons, isCartVisible, onPurchaseFinish } = useCart()
+
+  const currentCartValue = cartPokemons.reduce(
+    (priceAccumulator, { price, amount }) => priceAccumulator + price * amount,
+    0
+  )
+  const { theme } = useTheme()
+
+  const renderTitle = () => {
+    return cartPokemons.length
+      ? 'Pokémons no seu carrinho'
+      : 'Você ainda não adicionou nenhum pokemon no seu carrinho'
+  }
+
+  const onConfirmPurchase = () => {
+    onPurchaseFinish()
+    showSuccessModal({
+      message: 'Obrigado por comprar Pokémons.',
+      subtitle: 'Até a próxima compra!'
+    })
+  }
+
+  const showPurchaseModal = () => {
+    showSuccessModal({
+      message: 'Deseja realmente efetivar a compra?',
+      subtitle: `Valor total da compra: R$ ${currentCartValue}`,
+      isConfirm: true,
+      onConfirmClick: onConfirmPurchase
+    })
+  }
+
+  return (
+    <div className={isCartVisible ? 'open-cart ' : 'closed-cart'}>
+      <span className='cart-title' style={{ color: theme.colors.cart.text }}>
+        {renderTitle()}
+      </span>
+      <div className='divider'></div>
+      <div className='inner'>
+        {cartPokemons
+          .sort((a, b) => a.addedAt - b.addedAt)
+          .map((pokemon, key) => {
+            return <CartItem key={key} pokemon={pokemon} />
+          })}
+        <div className='current-cart-value'>
+          <div>Valor total no carrinho: </div>
+          R$ {currentCartValue}
+        </div>
+        {cartPokemons.length && (
+          <button
+            theme={theme}
+            onClick={showPurchaseModal}
+            disabled={currentCartValue === 0}
+          >
+            Finalizar Compra
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export { Cart }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { Cart } from './cart/cart.component'
+export { Header } from './header/header.component'
+export { Modal } from './modal/modal.component'
+export { Pagination } from './pagination/pagination.component'
+export { PokemonCard } from './pokemon-card/pokemon-card.component'
+export { SearchBar } from './search-bar/search-bar.component'
