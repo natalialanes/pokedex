d1f4774d05f6390bbcf2611f321720a705652a9e
Natalia Lanes <natalialanes@MB-004507.local>
2021-04-25 21:41:36 -0300

    added dashboard page

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -16,34 +16,33 @@ const Dashboard = () => {
 
   const pokemonTypes = [
     { electric: 'Elétrico', image: DashPikachu, alt: 'pikachu' },
-    { fire: 'Fogo', image: DashCharmander, alt: 'charmander' },
-    { grass: 'Grama', image: DashBulbassaur, alt: 'bulbassaur' },
-    { water: 'Água', image: DashSquirtle, alt: 'squirtle' }
+    { fire: 'Fogoso', image: DashCharmander, alt: 'charmander' },
+    { grass: 'Gramento', image: DashBulbassaur, alt: 'bulbassaur' },
+    { water: 'Aquático', image: DashSquirtle, alt: 'squirtle' }
   ]
 
   const renderPokemonOptions = () => {
     return pokemonTypes.map((type, key) => {
+      const pokemonKeyProperties = Object.keys(type)
+      const pokemonValueProperties = Object.values(type)
       return (
         <div
-          className={Object.keys(type)[0]}
-          onClick={() => handleRedirect(Object.keys(type)[0])}
+          className={pokemonKeyProperties[0]}
+          onClick={() => handleRedirect(pokemonKeyProperties[0])}
+          key={key}
         >
           <img
             className='dash-image'
-            src={Object.values(type)[1]}
-            alt={Object.values(type)[key]}
+            src={pokemonValueProperties[1]}
+            alt={pokemonValueProperties[key]}
           />
-          <div className='type'>{Object.values(type)[0]}</div>
+          <div className='type'>{pokemonValueProperties[0]}</div>
         </div>
       )
     })
   }
 
-  return (
-    <div className='dashboard'>
-      <div className='pokemon-options'>{renderPokemonOptions()}</div>
-    </div>
-  )
+  return <div className='pokemon-options'>{renderPokemonOptions()}</div>
 }
 
 export { Dashboard }
